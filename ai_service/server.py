7f3be6afb5c87f5e561e24ea3b0af91f24521943
arod381 <arod381@wgu.edu>
2026-08-10 20:15:17 -0500

    Updated server.py with OLLAMA model

--- ai_service/server.py
+++ ai_service/server.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+import requests
 
 app = FastAPI()
+
+OLLAMA_URL = "http://127.0.0.1:11434/api/generate"
+MODEL = "qwen3:1.7b"
 
 class ChatRequest(BaseModel):
     prompt: str
@@ -11,6 +15,20 @@ class ChatResponse(BaseModel):
 
 @app.post("/chat", response_model=ChatResponse)
 def chat(request: ChatRequest):
+
+    ollama_response = requests.post(
+        OLLAMA_URL,
+        json={
+            "model": MODEL,
+            "prompt": request.prompt,
+            "stream": False
+        }
+    )
+
+    ollama_response.raise_for_status()
+
+    data = ollama_response.json()
+
     return ChatResponse(
-        response=f"Python received: {request.prompt}"
+        response=data["response"]
     )
